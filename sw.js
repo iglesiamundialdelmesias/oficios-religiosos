5c347857420ab0e6576c90dc4ba99fee5102dfc5
iglesiamundialdelmesias <iglesiamundialdelmesias@gmail.com>
2026-09-23 21:07:16 +0000

    limpiar cache service worker

--- sw.js
+++ sw.js
@@ -1,19 +1,6 @@
-const CACHE_NAME = 'oficios-app-v1';
-const urlsToCache = [
-  './',
-  './index.html'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
   );
 });
